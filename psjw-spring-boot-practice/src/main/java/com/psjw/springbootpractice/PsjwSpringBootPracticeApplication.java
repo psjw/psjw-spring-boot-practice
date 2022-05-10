842388fc0bf588e841a16ad868aaf022754384b4
psjw <dlrudtn108@gmail.com>
2022-05-10 09:58:01 +0900

    Initialize #1 - spring boot
    스프링 부트 기본

---
+++ psjw-spring-boot-practice/src/main/java/com/psjw/springbootpractice/PsjwSpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.psjw.springbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsjwSpringBootPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsjwSpringBootPracticeApplication.class, args);
+    }
+
+}
---
+++ psjw-spring-boot-practice/src/test/java/com/psjw/springbootpractice/PsjwSpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psjw.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsjwSpringBootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
